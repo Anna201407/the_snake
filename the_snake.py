ceeb9f8a971d7553c385cb23014e1ece2ee69adb
Анна Панова <a07577585@gmail.com>
2026-04-27 17:45:04 +0300

    new class GameObject:

--- the_snake.py
+++ the_snake.py
@@ -43,73 +43,24 @@ clock = pygame.time.Clock()
 class GameObject:
     """
     Базовый класс, от которого наследуются все объекты.
-    содержит общие атрибут: позиция и цвет.
+    Содержит общие атрибуты: позиция и цвет.
     """
-
     def __init__(self, position=None, body_color=None):
         """
         Конструктор базового игрового объекта.
-        Аргументы: position (координаты), body_color(цвет).
+        Аргументы: position (координаты), baby_color (цвет).
         """
         if position is None:
-            self.position=(320, 240)
+            self.position = (320, 240)
         else:
-            self.position=position
+            self.boby_color = boby_color
     def draw(self, surface):
         """
-        Абстрактный метод для отрисовки объекта на экране.
-        Аргумент: surface(поверхность, на которой рисуем).
+        Метод для отрисовки объекта на экране.
+        Аргумент: surface (поверхность, на которой рисуем)
         """
         pass
-class Apple(GameObject):
-    """
-    Класс Apple наследуется от класса GameObject.
-    появляется в случайном месте поля.
-    """
-    super().__init__(position=Nove, body_color=APPLE_COLOR)
-
-    self.randomize_pocition(self):
-
-    """
-    Устанавливает координаты для яблока.
-    """
-    max_x = 640 - 20
-    max_y = 480 - 20
-
-    x.random.randrage(0, max_x + 1, 20)
-    y.random.randrage(0, max_y + 1, 20)
-
-    self.position = (x, y)
-
-def draw(self, surface):
-    """
-    Отрисовывает яблоко на игровом поле.
-    """
-    rect = pygame.Rect(
-        self.position[0],
-        self.position[1],
-        20,
-        20
-    )
-pygame.draw.rect(surface, self.body_color, rect)
-
-def main():
-    # Инициализация PyGame:
-    pygame.init()
-    # Тут нужно создать экземпляры классов.
-    ...
-
-    # while True:
-    #     clock.tick(SPEED)
-
-        # Тут опишите основную логику игры.
-        # ...
-
-
-if __name__ == '__main__':
-    main()
-
-
+        
 # Метод draw класса Apple
 # def draw(self):
 #     rect = pygame.Rect(self.position, (GRID_SIZE, GRID_SIZE))
